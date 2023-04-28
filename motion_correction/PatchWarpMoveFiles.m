29274dffe991ff9ddf3fdcfdb5e11651ea429339
CamiloG <78550048+CamiloGuevaraEsp@users.noreply.github.com>
2023-04-28 14:58:37 -0400

    Add files via upload
    It contains all the necessary codes to apply analysis of 2p imaging with XY motion correction. If you are drawing masks, it assumes you are not drawing slice for slice. For that go to folder "ROIs multislice"

---
+++ motion_correction/PatchWarpMoveFiles.m
@@ -0,0 +1,68 @@
+%% PatchWarMoveFile
+
+%Te only solution to apply patchwarp was to separate the files from
+%diferent slices, treat them as a single plane, apply Patchwarp and save
+%them in a separate folder. 
+%Run the first section before applying the code patchwarp_demo.m and apply
+%the second sections one it is done. 
+%First version by Camilo Guevara April 26th 2023
+%% Move the original files to separate folders defined by the slice
+clear all
+
+% Change the following variables
+num_slices = 3; 
+rootdir = '/Users/camilog/Downloads/test_images/TSeries-11082022-1509-1030';
+TImgRoot = 'TSeries-11082022-1509-1030'
+cd(rootdir)
+
+%Don't edit 
+tifflist = dir(['*tif'])
+num_elements = numel(tifflist);
+save num_elements
+
+for i = 1:num_slices
+    foldername = ['slice_' num2str(i, '%1.0f') '_folder']
+    display(foldername)
+    mkdir (foldername)
+    %cd(foldername)
+    for j = i: num_slices : numel(tifflist)
+        tiff_to_move = [TImgRoot '_Cycle00001_Ch2_' num2str(j, '%06.0f') '.ome.tif'];
+        movefile(tiff_to_move, foldername)
+    end
+
+end
+f = msgbox("Now go to Patchwarp :) "); 
+
+%% Moving the original files back to their folder once patchwarp is done
+
+rootdir = '/Users/camilog/Downloads/test_images/TSeries-11082022-1509-1030';
+load num_elements.mat
+TImgRoot = 'TSeries-11082022-1509-1030'
+for i = 1:num_slices
+    foldername = ['slice_' num2str(i, '%1.0f') '_folder']
+    slicedir = [rootdir '/' foldername];
+    %mkdir (foldername)
+    cd(slicedir)
+    for j = i: num_slices : num_elements
+        tiff_to_move = [TImgRoot '_Cycle00001_Ch2_' num2str(j, '%06.0f') '.ome.tif'];
+        movefile(tiff_to_move, rootdir)
+    end
+end
+
+%Moving all the corrected to a corrected folder 
+
+cd(rootdir)
+corrected_foldername = 'motionCorrected'; 
+mkdir(corrected_foldername);
+correctedDir = [rootdir '/' corrected_foldername]
+TImgRoot = 'TSeries-11082022-1509-1030'
+for i = 1:num_slices
+    foldername = ['slice_' num2str(i, '%1.0f') '_folder/corrected/post_warp']
+    slicedir = [rootdir '/' foldername];
+    %mkdir (foldername)
+    cd(slicedir)
+    for j = i: num_slices : num_elements
+        tiff_to_move = [TImgRoot '_Cycle00001_Ch2_' num2str(j, '%06.0f') '.ome_corrected_warped.tif'];
+        movefile(tiff_to_move, correctedDir)
+    end
+end
---
+++ motion_correction/write2PhotonTProjMask_correction.m
@@ -0,0 +1,136 @@
+%C.G. Interpretation of this code:
+% It loads the .tif images and create a z projection of it, then it define
+% a threshold based on the minimum, maximun and median pixel value, create
+% a binary mask of the z-projection based on that threshold. 
+%Saves the z-projection, the thesholded images and properties such as the
+%pixel centroid coordinate and area. 
+%Two questions:
+%1: Why is maxframes2read = 300
+%2: Why is the threshold defined in that way (line 105)? 
+% INPUTS: Cycle and channel of data to take a Z projection of.
+%OUTPUTS:
+%Saves a struct which contains "avgIntensity"
+%%numFrames: total # of frames.
+%%sumFrame: sum of intensities at each position.
+%
+% Not yet complete as of 05/02/21
+%By: Cynthia T. Hsu 
+
+function [sumFrame,numFrames] = write2PhotonTProjMask(varargin)
+
+% close all; clear all;
+if(nargin==0),
+    rootdir = '/Users/camilog/Downloads/test_images';
+    TSeriesFolder = 'TSeries-11082022-1509-1030';
+    TSeriesCorrectedFolder = 'motionCorrected';
+    %Only change variables above this line
+    %--------------------------------------------------
+    TImgRoot = [TSeriesFolder '_Cycle00001_Ch2_'];
+    %Cycle number indicates the time stamp.
+    %
+    
+    %The last time I used this script when I remembered what I was doing:
+    %     rootdir = 'D:\84C10Gal4_UASGCaMP_RFP_180607\'
+    %     TSeriesFolder = 'TSeries-06072018-1333-279';
+    %     TImgRoot = [TSeriesFolder '_Cycle00001_Ch2_'];
+else, %The variables we entered in above are being called by an external script.
+    rootdir = varargin{1};
+    TSeriesFolder = varargin{2};
+    TImgRoot = [TSeriesFolder '_Cycle00006_Ch' varargin{3} '_'];
+end;
+
+if(strfind(TImgRoot,'_Ch2_')),
+    %     img2mask = strrep(TImgRoot,'_Ch2_', '_Ch1_TprojMask.mat');
+    %     outname = [TImgRoot '_maskedByCh1.mat'];
+    outname = [TImgRoot '_mask.mat'];
+else,
+    img2mask = strrep(TImgRoot,'_Ch1_', '_Ch1_mask.mat');
+    outname = img2mask;
+end;
+
+maxNumFrames = 120*4*20; %120 seconds, 4 Hz, 20 slices - this is super important for the 90 minute recordings because otherwise this script will take forever to run when it really shouldn't.
+
+
+medfiltSize = 1;
+
+cd(rootdir);
+cd(TSeriesFolder);
+cd(TSeriesCorrectedFolder);
+% Last frame: 'fc2_save_2017-12-01-173304-10412';
+tiffList = dir([TImgRoot '*.tif']);
+display(TImgRoot);
+display(numel(tiffList));
+
+% maxFrame2read = 8548; %numel(tiffList);
+maxFrame2read = 300; %numel(tiffList);
+maxFrame2read = min(maxFrame2read,numel(tiffList));
+
+% regionPropsArray = cell(maxFrame2read,1);
+for(ti = 1:maxFrame2read),
+    if(mod(ti,15)>7),
+%         display(ti);
+%if corrected uncomment next line
+        imgName = [TImgRoot num2str(ti,'%06.0f') '.ome_corrected_warped.tif'];
+    
+%         try,
+%C.G. Apply a median filter to each image and then it sums it. Creating a
+%projection of the z-stack
+            rawImg = double(medfilt2(imread(imgName),[medfiltSize medfiltSize]));
+            %     rawImg_doubleVec =double(rawImg(:));
+            %     display(size(rawImg));
+            % dbquit
+            %         if(ti == 1),
+            if(~exist('allImgSum','var')),
+                
+                allImgSum =zeros(size(rawImg,1),size(rawImg,2));
+            end;
+            allImgSum = allImgSum+rawImg;
+            
+%         catch,
+%         end;
+    end;
+end;
+
+cd(rootdir);
+if(nargin==0),
+    avgImg = allImgSum/maxFrame2read;
+    [n,xout] = hist(avgImg(:),1:128);
+    figure; plot(xout,n);
+    medianPxVal = median(avgImg(:));
+    maxPxVal = max(avgImg(:));
+    minPxVal = min(avgImg(:));
+    % bwThresh = 2*medianPxVal/maxPxVal;
+    % try,
+    bwThresh = (medianPxVal+(medianPxVal-minPxVal))/maxPxVal;
+    if(bwThresh>1),
+        cumsum_n = cumsum(n/sum(n));
+        highIndex = find(cumsum_n>0.98,1);
+        bwThresh = xout(highIndex)/maxPxVal;
+    end;
+    
+    figure; imagesc(avgImg/maxPxVal);
+    
+    bwFrame = im2bw(avgImg/maxPxVal,bwThresh);
+    
+    s = regionprops(bwFrame>0,'Area','Centroid');
+    
+    imshow(uint8(avgImg));
+
+    %Shows the thresholded pixels 
+    figure; imshow(uint8(bwFrame*256));
+    % for(si = 1:numel(s)),
+    %     text(s(si).Centroid(1),s(si).Centroid(2),num2str(si),'r');
+    % end;
+    
+    % img2mask = strrep(TImgRoot,'_Ch2_', '_Ch1_mask.mat']);
+    save(outname,'avgImg','bwThresh','bwFrame','s');
+    imwrite(uint8(bwFrame*256),strrep(outname,'.mat','.tif'));
+else,
+    sumFrame = allImgSum;
+    numFrames = maxFrame2read;
+end;
+% catch,
+%     save(outname,'avgImg');
+% imwrite(strrep(outname,'.mat'),'.tif',uint8(bwFrame*256));
+%
+% end;
