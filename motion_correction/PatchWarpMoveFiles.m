86bf10e5f35cf2ea650064e589727fb728dbfc69
CamiloG <78550048+CamiloGuevaraEsp@users.noreply.github.com>
2023-05-02 17:14:32 -0400

    Update PatchWarpMoveFiles.m
    To increase speed: 
    1-Changed movefile to copyfile
    2-Included parfor
    
    As I am copying files there is no need to put the original files back

--- motion_correction/PatchWarpMoveFiles.m
+++ motion_correction/PatchWarpMoveFiles.m
@@ -20,35 +20,19 @@ tifflist = dir(['*tif'])
 num_elements = numel(tifflist);
 save num_elements
 
-for i = 1:num_slices
+parfor i = 1:num_slices
     foldername = ['slice_' num2str(i, '%1.0f') '_folder']
     display(foldername)
     mkdir (foldername)
     %cd(foldername)
     for j = i: num_slices : numel(tifflist)
         tiff_to_move = [TImgRoot '_Cycle00001_Ch2_' num2str(j, '%06.0f') '.ome.tif'];
-        movefile(tiff_to_move, foldername)
+        copyfile(tiff_to_move, foldername)
     end
 
 end
 f = msgbox("Now go to Patchwarp :) "); 
-
-%% Moving the original files back to their folder once patchwarp is done
-
-rootdir = '/Users/camilog/Downloads/test_images/TSeries-11082022-1509-1030';
-load num_elements.mat
-TImgRoot = 'TSeries-11082022-1509-1030'
-for i = 1:num_slices
-    foldername = ['slice_' num2str(i, '%1.0f') '_folder']
-    slicedir = [rootdir '/' foldername];
-    %mkdir (foldername)
-    cd(slicedir)
-    for j = i: num_slices : num_elements
-        tiff_to_move = [TImgRoot '_Cycle00001_Ch2_' num2str(j, '%06.0f') '.ome.tif'];
-        movefile(tiff_to_move, rootdir)
-    end
-end
-
+%% 
 %Moving all the corrected to a corrected folder 
 
 cd(rootdir)
@@ -56,13 +40,13 @@ corrected_foldername = 'motionCorrected';
 mkdir(corrected_foldername);
 correctedDir = [rootdir '/' corrected_foldername]
 TImgRoot = 'TSeries-11082022-1509-1030'
-for i = 1:num_slices
+parfor i = 1:num_slices
     foldername = ['slice_' num2str(i, '%1.0f') '_folder/corrected/post_warp']
     slicedir = [rootdir '/' foldername];
     %mkdir (foldername)
     cd(slicedir)
     for j = i: num_slices : num_elements
         tiff_to_move = [TImgRoot '_Cycle00001_Ch2_' num2str(j, '%06.0f') '.ome_corrected_warped.tif'];
-        movefile(tiff_to_move, correctedDir)
+        copyfile(tiff_to_move, correctedDir)
     end
 end
